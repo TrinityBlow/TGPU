e5b1ba0753b0e0efe470d645ff61add97852061a
TrinityBlow <franco.matteuci@gmail.com>
2017-09-20 15:51:05 -0300

    Add files via upload

--- Entrega1/ejercicio1.cu
+++ Entrega1/ejercicio1.cu
@@ -32,15 +32,13 @@ __global__ void ecuacion_kernel_outplace_p2(double *d_matA,double *d_matB,double
     int distA = blockIdx.y * blockDim.y + threadIdx.y; //i
     int distB = blockIdx.x * blockDim.x + threadIdx.x; //j
     int k;
-    if (distA*n+distB < n*n){
+    if (distA*n+distB < (n*n - 1)){
         //multiplicacion 
         for(k = 0; k < n ;k++){
             d_matC[distA*n+distB] += d_matA[distA*n+k] * d_matBT[distB+k*n];
         }
         //suma
-        if (distA*n+distB < n*n){
-            d_matC[distA*n+distB] += d_matB[distA*n+distB] + d_matAT[distA*n+distB];
-        }
+        d_matC[distA*n+distB] += d_matB[distA*n+distB] + d_matAT[distA*n+distB];
     }
 }
 __global__ void ecuacion_kernel_inplace (double *d_matA,double *d_matB,double *d_matC, unsigned int n){
@@ -48,45 +46,43 @@ __global__ void ecuacion_kernel_inplace (double *d_matA,double *d_matB,double *d
     int distB = blockIdx.x * blockDim.x + threadIdx.x; //j
     int k;
     //multiplicacion 
-    if (distA*n+distB < n*n){
+    if (distA*n+distB < (n*n - 1)){
         for(k = 0; k < n ;k++){
-            d_matC[distA*n+distB] += d_matA[distA*n+k] * d_matB[distB*n+k];
+            d_matC[distA*n+distB] += d_matA[distA*n+k] * d_matB[distB*n+k]; //multiplica d_matB por fila para simular la transpuesta
         }
-        if (distA*n+distB < n*n){
-            d_matC[distA*n+distB] += d_matB[distA*n+distB] + d_matA[distA+distB*n];
-        }
+	//suma
+        d_matC[distA*n+distB] += d_matB[distA*n+distB] + d_matA[distA+distB*n]; //d_matA busca por columna para simular la suma con la transpuesta
     }
+
 }
 
 void checkparams(unsigned long *n, unsigned int *cb);
 
 int main(int argc, char *argv[]){
 
-        if (argc != 2){
-            printf("Falta argumento: N\n");
+        if (argc != 3){
+            printf("Falta argumento: N, CUDABLK\n");
             return 0;
         }
+	//declaracion de variables
     cudaError_t error;
-
     unsigned int N = atoi (argv[1]);
-    unsigned int CUDA_BLK = 16, gridBlock;
+    unsigned long CUDA_BLK = atoi (argv[2]), gridBlock;
     unsigned long numBytes = sizeof(double)*N*N;
-    double *matA,*matB,*matC,*matAT,*d_matA,*d_matB,*d_matC,*d_matAT,*d_matBT,timetick;
+    double *matA,*matB,*matC,*d_matA,*d_matB,*d_matC,*d_matAT,*d_matBT,timetick;
     unsigned int i,j,k;
 
-
+	//inicializa variables para cpu
     matA = (double *)malloc(numBytes);
-    matAT = (double *)malloc(numBytes);
     matB = (double *)malloc(numBytes);
     matC = (double *)malloc(numBytes);
-
     for (i = 0; i < N*N; i++){
-        matA[i] = i;
-        matB[i] = i;
+        matA[i] = 2;
+        matB[i] = 3;
         matC[i] = 0;
-        matAT[i] = 0;
     }
 
+  //inicializa variables para gpu
   cudaMalloc((void **) &d_matA, numBytes);
   cudaMalloc((void **) &d_matAT, numBytes);
   cudaMalloc((void **) &d_matB, numBytes);
@@ -95,23 +91,16 @@ int main(int argc, char *argv[]){
   cudaMemcpy(d_matA, matA, numBytes, cudaMemcpyHostToDevice); // CPU -> GPU
   cudaMemcpy(d_matB, matB, numBytes, cudaMemcpyHostToDevice); // CPU -> GPU
   cudaMemcpy(d_matC, matC, numBytes, cudaMemcpyHostToDevice); // CPU -> GPU
-  gridBlock = (unsigned int)sqrt(N*N/CUDA_BLK/CUDA_BLK);
 
-  // Bloque bidimencional de hilos (*cb* hilos)
-  dim3 dimBlock(CUDA_BLK,CUDA_BLK);
-  // Grid bidimencional (*ceil(n/cb)* bloques)
-  dim3 dimGrid(gridBlock,gridBlock);
+  gridBlock = (unsigned int)sqrt(N*N/CUDA_BLK/CUDA_BLK);
+  dim3 dimBlock(CUDA_BLK,CUDA_BLK); // Bloque bidimencional de hilos (*cb* hilos)
+  dim3 dimGrid(gridBlock,gridBlock); // Grid bidimencional (*ceil(n/cb)* bloques)
 
     //--------------------------------cpu comienza ------------------------------------
 
     //secuencial
 	timetick = dwalltime();
-    //transpuesta out-place A
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            matAT [j*N + i] = matA[i*N + j];
-        }
-    }
+
     //multiplicacion
     for(i = 0; i < N; i++){
         for(j = 0; j < N; j++){
@@ -123,9 +112,10 @@ int main(int argc, char *argv[]){
     //suma
     for(i = 0; i < N; i++){
         for(j = 0; j < N; j++){
-            matC[i*N+j] += matB[i*N+j] + matAT[i*N+j];
+            matC[i*N+j] += matB[i*N+j] + matA[i+j*N];
         }
     }
+
 	printf("Tiempo para la ecuacion CPU: %f\n\n",dwalltime() - timetick);
 
     //--------------------------------cpu termina ------------------------------------
@@ -136,7 +126,6 @@ int main(int argc, char *argv[]){
         matA[i] = i;
         matB[i] = i;
         matC[i] = 0;
-        matAT[i] = 0;
     }
 
     //--------------------------------gpu out-place comienza ------------------------------------
@@ -169,36 +158,8 @@ int main(int argc, char *argv[]){
     //--------------------------------gpu in-place termina ------------------------------------
 
     cudaMemcpy(matC, d_matC, numBytes, cudaMemcpyDeviceToHost); // GPU -> CPU
-    cudaMemcpy(matA, d_matA, numBytes, cudaMemcpyDeviceToHost); // GPU -> CPU
-    cudaMemcpy(matB, d_matB, numBytes, cudaMemcpyDeviceToHost); // GPU -> CPU
-    cudaMemcpy(matAT, d_matAT, numBytes, cudaMemcpyDeviceToHost); // GPU -> CPU
   
-/*
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            printf("%f|",matA[i*N+j]);
-        }
-        printf("\n");
-    }
-	printf("\n");
-
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            printf("%f|",matB[i*N+j]);
-        }
-        printf("\n");
-    }
-	printf("\n");
-
-    for(i = 0; i < N; i++){
-        for(j = 0; j < N; j++){
-            printf("%f|",matAT[i*N+j]);
-        }
-        printf("\n");
-    }
-	printf("\n");
-
-
+/*  //imprime la matriz matC
     for(i = 0; i < N; i++){
         for(j = 0; j < N; j++){
             printf("%f|",matC[i*N+j]);
@@ -216,9 +177,16 @@ int main(int argc, char *argv[]){
     free(matA);
     free(matB);
     free(matC);
-    free(matAT);
     return 0;
 }
+
+
+
+
+
+
+
+
 
 void checkparams(unsigned long *n, unsigned int *cb){
   struct cudaDeviceProp capabilities;
